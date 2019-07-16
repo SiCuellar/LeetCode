b72231ee591ea82cae3debc718cfecb4a9a8e46f
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-07-16 13:08:53 -0700

    added in place solution

--- move_zeros/mv.rb
+++ move_zeros/mv.rb
@@ -13,3 +13,15 @@ class Mv
     sorted_nums.rotate(count)
   end
 end
+
+#in place solution 
+# def move_zeros(nums)
+#   return if nums.length < 2
+#   current = -1
+#   nums.each_with_index do |num, i|
+#     next if num == 0
+#     current += 1
+#     nums[i] = nums[current]
+#     nums[current] = num
+#   end
+# end
