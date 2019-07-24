8891f7c9dc63ae2d5b0c1d75c85f14880a347b48
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-07-24 13:25:08 -0600

    roman numeral converter working

--- roman_to_int/rom_to_int.rb
+++ roman_to_int/rom_to_int.rb
@@ -1,33 +1,55 @@
 require 'pry'
 
 class RomToInt
-  def roman_to_int(roman_int)
-    map = {
-      "I" => 1,
-      "V" => 5,
-      "X" => 10,
-      "L" => 50,
-      "C" => 100,
-      "D" => 500,
-      "M" => 1000,
-    }
 
-    values = []
+  Map = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 1000
+  }
 
-    roman_int.chars.each do |char|
-      values << map[char]
+  def roman_to_int(s)
+    res = 0
+    temp = 0
+    s.chars.each_with_index do |element, i|
+      if Map[s[i + 1]] && Map[element] < Map[s[i+1]]
+        temp = Map[element]
+      else
+        res += (Map[element] - temp)
+        temp = 0
+      end
     end
-
-    max_value_index = values.each_with_index.max[1]
-
-    if max_value_index != 0 && values.sum > 3
-      neg = values.slice_before(values.max).to_a.first.sum
-      pos = values.slice_before(values.max).to_a[1].sum
-      pos - neg
-    else
-      values.sum
-    end
+    res
   end
+#   map = {
+#     "I" => 1,
+#     "V" => 5,
+#     "X" => 10,
+#     "L" => 50,
+#     "C" => 100,
+#     "D" => 500,
+#     "M" => 1000,
+#   }
+#   def roman_to_int(roman_int)
+#     values = []
+#
+#     roman_int.chars.each do |char|
+#       values << map[char]
+#     end
+#     max_value_index = values.each_with_index.max[1]
+#
+#     if max_value_index != 0 && values.sum > 3
+#       neg = values.slice_before(values.max).to_a.first.sum
+#       pos = values.slice_before(values.max).to_a[1].sum
+#       pos - neg
+#     else
+#       values.sum
+#     end
+#   end
 end
 
 
