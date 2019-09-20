c3ec60ed0457f6b8cc1fd54b981b0370a36f222b
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-09-20 00:20:28 -0600

    working longest common prefix for two words

--- Ruby/longest_common_prefix/lcp.rb
+++ Ruby/longest_common_prefix/lcp.rb
@@ -1,5 +1,19 @@
 require "pry"
 
 class Lcp
+  def word_parser(word)
+    word.chars
+  end
 
+  def word_compare(first_w, sec_w)
+    common_prefix = []
+    first_letters = word_parser(first_w)
+    second_letters = word_parser(sec_w)
+    first_letters.each_with_index do |let, index|
+      if let == second_letters[index]
+        common_prefix << let
+      end
+    end
+    common_prefix.join
+  end
 end
--- Ruby/longest_common_prefix/lcp_test.rb
+++ Ruby/longest_common_prefix/lcp_test.rb
@@ -7,4 +7,20 @@ class LcpTest < Minitest::Test
     lcp = Lcp.new
     assert_instance_of Lcp,lcp
   end
+
+  def test_it_can_split_word_into_chars
+    lcp = Lcp.new
+    assert_equal ["c","a","r"], lcp.word_parser("car")
+  end
+
+  def test_it_can_compare_two_words
+    lcp = Lcp.new
+    assert_equal "ca", lcp.word_compare("car","cat")
+  end
+
+  def test_it_can_compare_three_words
+    lcp = Lcp.new
+    words = ["cards", "carpet",  "carter"]
+    assert_equal "car", lcp.longest_common_pre(words)
+  end
 end
