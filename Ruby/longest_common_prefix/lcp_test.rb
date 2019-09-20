7dd46b707e1f11842c0e977fb80bc59923a1c914
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-09-19 23:27:35 -0600

    working on longest common prefix

--- Ruby/longest_common_prefix/lcp_test.rb
+++ Ruby/longest_common_prefix/lcp_test.rb
@@ -2,9 +2,9 @@ require_relative "lcp.rb"
 require "minitest/autorun"
 require "minitest/pride"
 
-class Lcp < Minitest::Test
+class LcpTest < Minitest::Test
   def test_it_exists
     lcp = Lcp.new
-    assert_instance_of Lcp,lcp 
+    assert_instance_of Lcp,lcp
   end
 end
