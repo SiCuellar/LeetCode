41935f67374e8423144b2923148f0dc6ec12a09c
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-09-24 10:57:08 -0700

    initial odd even jump

---
+++ Python/odd_even_jump/ruby/odd_even_jump_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "odd_even_jump"
+
+class OddEvenJumpTest < Minitest::Test
+  def test_it_exists
+    odd_even_jump = OddEvenJump.new
+    assert_instance_of OddEvenJump, odd_even_jump
+  end
+end
