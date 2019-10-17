97e1c7783bd8be5faa652221351d20c64a87d1ce
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-10-17 14:52:19 -0600

    adding jewel and stone checker

---
+++ Ruby/jewels_and_stones/jewel_stone.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+class JewelStone
+  def num_jewels_in_stones(j, s)
+    binding.pry
+  end
+
+  def num_jewels(j, s)
+    j
+  end
+
+  def num_stones(j, s)
+    s
+  end
+end
---
+++ Ruby/jewels_and_stones/jewel_stone_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "jewel_stone"
+
+class JewelStoneTest < Minitest::Test
+  def test_it_exists
+    jewel_stone = JewelStone.new
+    assert_instance_of JewelStone, jewel_stone
+  end
+
+  def test_it_has_attributes
+    jewel_stone = JewelStone.new
+    j = "a"
+    s = "aAbc"
+    assert_equal "a", jewel_stone.num_jewels(j, s)
+    assert_equal "aAbc", jewel_stone.num_stones(j, s)
+  end
+end
