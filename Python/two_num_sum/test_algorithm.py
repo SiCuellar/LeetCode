375cb61ac084489e2e48e73428cf080f97829976
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-04 19:57:06 -0700

    added further testing

--- Python/two_num_sum/test_algorithm.py
+++ Python/two_num_sum/test_algorithm.py
@@ -5,5 +5,8 @@ class TestAlgorithm(unittest.TestCase):
     def test_case_1(self):
         self.assertEqual(algorithm.twoNumberSum([4, 6], 10), [4, 6])
 
+    def test_case_2(self):
+        self.assertEqual(algorithm.twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17), [8, 9])
+
 if __name__ == "__main__":
     unittest.main()
