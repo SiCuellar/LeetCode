4ea09071a7c21956213a68978d7b87e22113436e
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-05 23:16:11 -0700

    different solution for two num sum

--- Python/two_num_sum/algorithm.py
+++ Python/two_num_sum/algorithm.py
@@ -1,12 +1,26 @@
 # O(n^2) time
 # 0(1) space
 
-def twoNumberSum(array, targetSum):
-    for i in range(len(array)-1):
-        firstNum = array[i]
-        for j in range(i + 1, len(array)):
-            secondNum = array[j]
-            if firstNum + secondNum == targetSum:
-                return [firstNum, secondNum]
-    return []
+# def twoNumberSum(array, targetSum):
+#     for i in range(len(array)-1):
+#         firstNum = array[i]
+#         for j in range(i + 1, len(array)):
+#             secondNum = array[j]
+#             if firstNum + secondNum == targetSum:
+#                 return [firstNum, secondNum]
+#     return []
 #-------------------------------------------------------
+
+# O(n) time
+# O(n) space
+
+def twoNumberSum(array, targetSum):
+    nums = {}
+
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    return []
