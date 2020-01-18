b36795a94454a3fd7fe509f37f61bae0f76d9567
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-01-18 14:21:02 -0700

    added further testig

--- Ruby/Hacker/Ransom_note/ransom_note.rb
+++ Ruby/Hacker/Ransom_note/ransom_note.rb
@@ -2,6 +2,19 @@ require 'pry'
 
 class Program
   def check_magazine(magazine, note)
-    magazine
+    words_present =
+    note.map do |word|
+      if magazine.include?(word)
+        "Yes"
+      else
+        "No"
+      end
+    end
+
+    if words_present.include?("No")
+      "No"
+    else
+      "Yes"
+    end
   end
 end
--- Ruby/Hacker/Ransom_note/ransom_note_test.rb
+++ Ruby/Hacker/Ransom_note/ransom_note_test.rb
@@ -9,13 +9,17 @@ class ProgramTest < Minitest::Test
   end
 
   def test_check_mag_can_input_a_mag_and_note
+    skip
     program = Program.new
     magazine = ["give", "one"]
     note = ["give"]
     assert_equal ["give", "one"], program.check_magazine(magazine, note)
   end
 
-  def test_it_can_make_a_dict_with_magazine
-    
+  def test_it_can_check_if_note_is_included_in_magazine
+    program = Program.new
+    magazine = ["give", "one"]
+    note = ["give"]
+    assert_equal "Yes", program.check_magazine(magazine, note)
   end
 end
